9e490c3a57b6f22c63ba5ca77f12a049eb133582
Talhaa-129 <talhakhan5023@gmail.com>
2023-03-28 05:01:31 +0500

    update new

--- src/config/database.typeorm.ts
+++ src/config/database.typeorm.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: '18.221.99.126',
   port: 5432,
   username: 'postgres',
   password: 'password',
   database: 'property_management',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'], // for local use
-  //   entities: ['dist/**/*.entity{.ts,.js}'], // for deployment
+  // entities: [__dirname + '/../**/*.entity.{js,ts}'], // for local use
+  entities: ['dist/**/*.entity{.ts,.js}'], // for deployment
   synchronize: true,
 };
