e0e8472e3fd9301f854b812d5c560436797d37cd
Talhaa-129 <talhakhan5023@gmail.com>
2023-03-28 05:03:18 +0500

    update newwww

--- dist/config/database.typeorm.js
+++ dist/config/database.typeorm.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeOrmConfig = void 0;
 exports.typeOrmConfig = {
     type: 'postgres',
-    host: 'localhost',
+    host: '18.221.99.126',
     port: 5432,
     username: 'postgres',
     password: 'password',
     database: 'property_management',
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
 };
 //# sourceMappingURL=database.typeorm.js.map
