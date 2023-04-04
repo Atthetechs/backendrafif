34243b44b1619235df886f2fd1fa60c03d166e57
Talhaa-129 <talhakhan5023@gmail.com>
2023-04-04 23:29:20 +0500

    token expiration

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '2m' },
+      signOptions: { expiresIn: '1440m' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
